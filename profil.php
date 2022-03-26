107b0b3657a05b8f54a5316e0e870da512a1865a
yanis-khiter <yanis.khiter@laplateforme.io>
2022-03-26 18:30:51 +0100

    finalisation du module

--- profil.php
+++ profil.php
@@ -41,50 +41,57 @@ if (isset($_SESSION['id_membre']) and $_SESSION['id_membre'] > 0) {
         $statut = 1;
         $id = $_SESSION['id_membre'];
 
-        $valid = (bool) true;
+        $valid_email = true; 
+        $valid_mdp = true; 
+        $valid_pseudo = true;
+        $valid_prenom = true; 
+        $valid_nom = true; 
+        $valid_ville = true; 
+        $valid_code_postale = true; 
+        $valid_adresse = true;
+               //  EMAIL
+
+        
+       $reqemail = $bdd->prepare("SELECT * FROM membre WHERE email =:email");
+       $reqemail->setFetchMode(PDO::FETCH_ASSOC);
+       $reqemail->execute(['email' => $email]);
+
+       $resultemail = $reqemail->fetch();
 
 
-        //  EMAIL
-
-        $reqmail = $bdd->prepare("SELECT * FROM membre WHERE email =:email");
-        $reqmail->setFetchMode(PDO::FETCH_ASSOC);
-        $reqmail->execute(['email' => $email]);
-
-        $resultmail = $reqmail->fetch();
-
-        if (empty($email)) {
-            $valid = false;
-            $err_email = "Renseignez l'email.";
-        } elseif (filter_var($email, FILTER_VALIDATE_EMAIL) == false) {
-            $valid = false;
+       if (empty($email)) {
+           $valid_email = false;
+           $err_email = "Renseignez votre mail.";
+       } elseif (filter_var($email, FILTER_VALIDATE_EMAIL) == false) {
+            $valid_email = false;
             $err_email = "Votre email n'est pas au bon format";
             $email = "";
-        } elseif ($resultmail) {
-            if($email == $email_origine) {
-                $valid = true;
-            } else {
-                $valid = false;
-                $err_email = "Cette adresse mail est déjà utilisée.";
-                $email ="";
-            }
-        }
+        } elseif ($resultemail) {
+           if($email == $email_origine) {
+               $valid_email = true;
+           } else {
+               $valid_email = false;
+               $err_email = "Cet mail est déjà utilisé.";
+               $email ="";
+           }
+       }
 
-   
+        
     
         //  MOT DE PASSE
 
         if (empty($mdp)) {
-            $valid = false;
+            $valid_mdp = false;
             $err_mdp = "Renseignez votre mot de passe.";
         } elseif (strlen($mdp) < 8) {
-            $valid = false;
+            $valid_mdp = false;
             $err_mdp = "Le mot de passe doit être de 8 caractères minimum.";
             $mdp = "";
         } elseif (empty($mdpconfirm)) {
-            $valid = false;
+            $valid_mdp = false;
             $err_mdpconfirm = "Confirmez votre mot de passe.";
         } elseif ($mdp !== $mdpconfirm) {
-            $valid = false;
+            $valid_mdp = false;
             $err_mdpconfirm = "Les mots de passes ne sont pas identiques !";
             $mdpconfirm = "";
         }
@@ -98,28 +105,28 @@ if (isset($_SESSION['id_membre']) and $_SESSION['id_membre'] > 0) {
 
         $resultpseudo = $reqpseudo->fetch();
 
+
         if (empty($pseudo)) {
-            $valid = false;
+            $valid_pseudo = false;
             $err_pseudo = "Renseignez votre pseudo.";
         } elseif ($resultpseudo) {
             if($pseudo == $pseudo_origine) {
-                $valid = true;
+                $valid_pseudo = true;
             } else {
-                $valid = false;
+                $valid_pseudo = false;
                 $err_pseudo = "Ce pseudo est déjà utilisé.";
                 $pseudo ="";
             }
         }
 
 
-
         // PRENOM
 
         if (empty($prenom)) {
-            $valid = false;
+            $valid_prenom = false;
             $err_prenom = "Renseignez votre prénom.";
         } elseif (!preg_match("#^[a-zA-Z]+$#", $prenom)) {
-            $valid = false;
+            $valid_prenom = false;
             $err_prenom = "Votre prénom ne doit pas contenir de chiffres ou de caractères spéciaux.";
             $prenom = "";
         }
@@ -128,10 +135,10 @@ if (isset($_SESSION['id_membre']) and $_SESSION['id_membre'] > 0) {
         //  NOM
 
         if (empty($nom)) {
-            $valid = false;
+            $valid_nom = false;
             $err_nom = "Renseignez votre nom.";
         } elseif (!preg_match("#^[a-zA-Z]+$#", $nom)) {
-            $valid = false;
+            $valid_nom = false;
             $err_nom = "Votre nom ne doit pas contenir de chiffres ou de caractères spéciaux.";
             $nom = "";
         }
@@ -140,10 +147,10 @@ if (isset($_SESSION['id_membre']) and $_SESSION['id_membre'] > 0) {
         // VILLE
 
         if (empty($ville)) {
-            $valid = false;
+            $valid_ville = false;
             $err_ville = "Renseignez votre ville.";
         } elseif (!preg_match("#^[a-zA-Z]+$#", $ville)) {
-            $valid = false;
+            $valid_ville = false;
             $err_ville = "Votre ville ne doit pas contenir de chiffres ou de caractères spéciaux.";
             $ville = "";
         }
@@ -152,10 +159,10 @@ if (isset($_SESSION['id_membre']) and $_SESSION['id_membre'] > 0) {
         // CODE POSTALE
 
         if (empty($code_postale)) {
-            $valid = false;
+            $valid_code_postale = false;
             $err_code_postale = "Renseignez votre code postal.";
         } elseif (!preg_match("~^[0-9]{5}$~", $code_postale)) {
-            $valid = false;
+            $valid_code_postale = false;
             $code_postale = '';
             $err_code_postale = "Le code postal n'est pas au bon format";
         }
@@ -164,13 +171,12 @@ if (isset($_SESSION['id_membre']) and $_SESSION['id_membre'] > 0) {
         // ADRESSE
 
         if (empty($adresse)) {
-            $valid = false;
-            $err_adresse = "Renseignez votre adresse..";
+            $valid_adresse = false;
+            $err_adresse = "Renseignez votre adresse.";
         }
 
 
-
-        if ($valid == true) {
+        if ($valid_email == true && $valid_mdp == true && $valid_pseudo == true && $valid_prenom == true && $valid_nom == true && $valid_ville == true && $valid_code_postale == true && $valid_adresse) {
 
             $req = $bdd->prepare("UPDATE `membre` SET `pseudo`= :pseudo,`mdp`= :mdp,`nom`= :nom,`prenom`= :prenom,`email`= :email,`ville`= :ville,`code_postale`= :code_p,`adresse`= :adresse,`statut`= 1 WHERE id_membre = :id");
             $req->bindValue(':pseudo', $pseudo);
@@ -211,15 +217,15 @@ if (isset($_SESSION['id_membre']) and $_SESSION['id_membre'] > 0) {
                     <div class="info">
 
                         <label>PRENOM</label>
-                        <?php if (isset($err_prenom)) {echo "<div class='err_form'> $err_prenom<div>";} ?>
+                        <?php if (isset($err_prenom)) {echo "<div class='error'> $err_prenom</div>";} ?>
                         <input type="text" name="prenom" value=<?php echo $userinfo['prenom']; ?> required>
 
                         <label>NOM</label>
-                        <?php if (isset($err_nom)) {echo "<div class='err_form'> $err_nom<div>";} ?>
+                        <?php if (isset($err_nom)) {echo "<div class='error'> $err_nom</div>";} ?>
                         <input type="text" name="nom" value=<?php echo $userinfo['nom']; ?> required>
 
                         <label>PSEUDO</label>
-                        <?php if (isset($err_pseudo)) {echo "<div class='err_form'> $err_pseudo<div>";} ?>
+                        <?php if (isset($err_pseudo)) {echo "<div class='error'> $err_pseudo</div>";} ?>
                         <input type="text" name="pseudo" value=<?php echo $userinfo['pseudo']; ?> required>
                         <input type="hidden" name="pseudo_origine" value=<?php echo $userinfo['pseudo']; ?> >
                     </div>
@@ -228,16 +234,16 @@ if (isset($_SESSION['id_membre']) and $_SESSION['id_membre'] > 0) {
                     <div class="connexion">
 
                         <label>EMAIL</label>
-                        <?php if (isset($err_email)) {echo "<div class='err_form'> $err_email<div>";} ?>
-                        <input type="email" name="email" value=<?php echo $userinfo['email']; ?> required>
+                        <?php if (isset($err_email)) {echo "<div class='error'> $err_email</div>";} ?>
+                        <input type="text" name="email" value=<?php echo $userinfo['email']; ?> required>
                         <input type="hidden" name="email_origine" value=<?php echo $userinfo['email']; ?> >
 
                         <label>MOT DE PASSE</label>
-                        <?php if (isset($err_mdp)) {echo "<div class='err_form'> $err_mdp<div>";} ?>
+                        <?php if (isset($err_mdp)) {echo "<div class='error'> $err_mdp</div>";} ?>
                         <input type="password" name="mdp" value=<?php echo $userinfo['mdp']; ?> required>
 
                         <label>CONFIRMATION MOT DE PASSE</label>
-                        <?php if (isset($err_mdpconfirm)) {echo "<div class='err_form'> $err_mdpconfirm<div>";} ?>
+                        <?php if (isset($err_mdpconfirm)) {echo "<div class='error'> $err_mdpconfirm</div>";} ?>
                         <input type="password" name="mdpconfirm" value=<?php echo $userinfo['mdp']; ?> required>
 
                     </div>
@@ -247,15 +253,15 @@ if (isset($_SESSION['id_membre']) and $_SESSION['id_membre'] > 0) {
                     <div class="adresse">
 
                         <label>ADRESSE</label>
-                        <?php if (isset($err_adresse)) {echo "<div class='err_form'> $err_adresse<div>";} ?>
+                        <?php if (isset($err_adresse)) {echo "<div class='error'> $err_adresse</div>";} ?>
                         <input type="text" name="adresse" value=<?php echo $userinfo['adresse']; ?> required>
 
                         <label>CODE POSTALE</label>
-                        <?php if (isset($err_code_postale)) {echo "<div class='err_form'> $err_code_postale<div>";} ?>
+                        <?php if (isset($err_code_postale)) {echo "<div class='error'> $err_code_postale</div>";} ?>
                         <input type="text" minlength="5" maxlength="5" name="code_postale" value=<?php echo $userinfo['code_postale']; ?> required>
 
                         <label>VILLE</label>
-                        <?php if (isset($err_ville)) {echo "<div class='err_form'> $err_ville<div>";} ?>
+                        <?php if (isset($err_ville)) {echo "<div class='error'> $err_ville</div>";} ?>
                         <input type="text" name="ville" value=<?php echo $userinfo['ville']; ?> required>
 
                     </div>
